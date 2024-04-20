d60d8ea859b825f26d6e2ecbe2d8acd687a8c92b
AvinashPotnuru1 <avinashpotnuru@techarion.com>
2024-04-20 17:53:17 +0530

    hello world component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <HelloWorld/>
+  <ButtonComponent/>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import ButtonComponent from "./components/Button.vue"
+
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ButtonComponent
   }
 }
 </script>
---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+<h1>button</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"ButtonComponent"
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
